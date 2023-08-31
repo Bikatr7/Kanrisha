de02a08e007fd20c9d45621cf1fd9c77da413a07
Alan Tommy <1750801177a@gmail.com>
2023-08-31 04:57:31 -0400

    Moved on_interaction from slashCommandHandler.py to eventHandler.py

--- handlers/eventHandler.py
+++ handlers/eventHandler.py
@@ -40,3 +40,46 @@ class eventHandler:
         @kanrisha_client.event
         async def on_message(message):
             print(f"{message.author}: {message.content}")
+
+         ##-------------------start-of-on_interaction()--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        @kanrisha_client.event
+        async def on_interaction(interaction: discord.Interaction):
+
+            """
+
+            Handles button interactions.\n
+
+            Parameters:\n
+            self (object - slashCommandHandler) : the slashCommandHandler object.\n
+            interaction (object - discord.Interaction) : the interaction object.\n
+
+            Returns:\n
+            None.\n
+
+            """
+
+            ## check if it's a button press
+            if(interaction.type == discord.InteractionType.component):
+
+                ## get the custom id of the button
+                custom_id = interaction.data.get("custom_id") if interaction.data else None
+
+                ## if register button was pressed by the correct user
+                if custom_id == f"register_{interaction.user.id}":
+
+                    ## acknowledge the interaction immediately
+                    await interaction.response.defer()
+
+                    await kanrisha_client.member_handler.add_new_member(interaction.user.id)
+
+                    await interaction.delete_original_response()
+
+                    await interaction.followup.send("You have been registered.", ephemeral=True)
+
+                ## if register button was pressed by the wrong user
+                elif custom_id and custom_id.startswith("register_"):
+
+                    await kanrisha_client.interaction_handler.send_response_no_filter_channel(interaction, "You are not authorized to use this button.", delete_after=5.0, is_ephemeral=True)
+
+
