c36349d10ef6783911dcb60c96bb0bc417852baa
Seinu <Seinuve@gmail.com>
2023-09-16 20:51:17 -0600

    moved role persist

--- modules/fileEnsurer.py
+++ modules/fileEnsurer.py
@@ -184,7 +184,7 @@ class fileEnsurer:
 
       self.banned_links_path = os.path.join(self.moderation_dir, "banned links.txt")
 
-      self.role_persistence_path = os.path.join(self.lib_dir, "role persistence.json")
+      self.role_persistence_path = os.path.join(self.moderation_dir, "role persistence.json")
 
       await self.file_handler.standard_create_directory(self.moderation_dir)
 
