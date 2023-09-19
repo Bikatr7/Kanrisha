8a83d20661592cb4a2134b4231ca3d04dea95c0f
Seinu <Seinuve@gmail.com>
2023-09-18 23:24:12 -0600

    added card.py

---
+++ entities/card.py
@@ -0,0 +1,29 @@
+## built-in libraries
+import typing
+
+class card:
+
+    """
+    
+    Represents a gacha card.\n
+
+    """
+
+##-------------------start-of-__init__()--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    def __init__(self):
+
+        """
+        
+        Initializes the card object.\n
+
+        Parameters:\n
+        None.\n
+
+        Returns:\n
+        None.\n
+
+        """
+
+        pass
